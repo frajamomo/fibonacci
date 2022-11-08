15a7c33f00bf239703b140922f2899588d93a740
frajamomo <frajamomo@hotmail.com>
2022-11-08 18:33:27 +0100

    delete blanks

--- test_fibonaccy.py
+++ test_fibonaccy.py
@@ -10,7 +10,7 @@ class Testing(unittest.TestCase):
 
     def test_high_value(self):
         self.assertEquals(fibonacci(100)[-1], 218922995834555169026)
-        
+
     def test_wrong_raises(self):
         with self.assertRaises(ValueError) as context:
             fibonacci("wrong")
