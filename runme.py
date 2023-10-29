3433e814dd06e8d101dd9994e962165c57ce317f
Francisco Javier Moreno <frajamomo@hotmail.com>
2023-10-29 19:06:51 +0100

    Refactor runme.py

--- runme.py
+++ runme.py
@@ -3,19 +3,14 @@ from fibonacciSequence import FibonacciSequence
 
 if __name__ == '__main__':
 
+    algorithms = [Iterative_while_loop(),
+                  Recursive(),
+                  Iterative_for_loop()]
+
     numberOfElements = int(input("Enter number of elements : "))
 
-    algorithm = Iterative_while_loop()
-    fibonacci = FibonacciSequence(algorithm)
-    print('{0: <50}'.format('Fibonacci sequence (' + algorithm.__class__.__name__ + ') : '),
-          fibonacci.generate(numberOfElements))
+    for algorithm in algorithms:
+        fibonacci = FibonacciSequence(algorithm)
+        print('{0: <50}'.format('Fibonacci sequence (' + algorithm.__class__.__name__ + ') : '),
+              fibonacci.generate(numberOfElements))
 
-    algorithm = Recursive()
-    fibonacci = FibonacciSequence(algorithm)
-    print('{0: <50}'.format('Fibonacci sequence (' + algorithm.__class__.__name__ + ') : '),
-          fibonacci.generate(numberOfElements))
-
-    algorithm = Iterative_for_loop()
-    fibonacci = FibonacciSequence(algorithm)
-    print('{0: <50}'.format('Fibonacci sequence (' + algorithm.__class__.__name__ + ') : '),
-          fibonacci.generate(numberOfElements))
