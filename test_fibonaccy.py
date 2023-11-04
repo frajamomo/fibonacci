4972c721c54a20e79f6301d4cc1baacc5a5beb80
Francisco Javier Moreno <frajamomo@hotmail.com>
2023-11-04 22:52:17 +0100

    Testing refactor to use inheritance

--- test_fibonaccy.py
+++ test_fibonaccy.py
@@ -2,84 +2,48 @@ import unittest
 from fibonacci import IterativeWhileLoop, IterativeForLoop, Recursive, Yield
 from fibonacciSequence import FibonacciSequence
 
+class AbstractFibonacci(object):
 
-class TestingIterativeWhileLoop(unittest.TestCase):
+    def test_empty_value(self):
+        self.assertEqual(self.fibonacci.generate(0), [])
+
+    def test_initial_value(self):
+        self.assertEqual(self.fibonacci.generate(1), [0])
+
+    def test_second_value(self):
+        self.assertEqual(self.fibonacci.generate(2), [0, 1])
+
+    def test_high_value(self):
+        self.assertEqual(self.fibonacci.generate(100)[-1], 218922995834555169026)
+        self.assertEqual(len(self.fibonacci.generate(100)), 100)
+
+    def test_wrong_raises(self):
+        with self.assertRaises(ValueError) as context:
+            self.fibonacci.generate("wrong")
+        self.assertEqual("Invalid input", str(context.exception))
+
+
+class TestingIterativeWhileLoop(AbstractFibonacci, unittest.TestCase):
 
     def setUp(self):
         self.fibonacci = FibonacciSequence(IterativeWhileLoop())
 
-    def test_empty_value(self):
-        self.assertEqual(self.fibonacci.generate(0), [])
 
-    def test_initial_value(self):
-        self.assertEqual(self.fibonacci.generate(1), [0])
-
-    def test_high_value(self):
-        self.assertEqual(self.fibonacci.generate(100)[-1], 218922995834555169026)
-
-    def test_wrong_raises(self):
-        with self.assertRaises(ValueError) as context:
-            self.fibonacci.generate("wrong")
-        self.assertEqual("Invalid input", str(context.exception))
-
-
-class TestingIterativeForLoop(unittest.TestCase):
+class TestingIterativeForLoop(AbstractFibonacci, unittest.TestCase):
 
     def setUp(self):
         self.fibonacci = FibonacciSequence(IterativeForLoop())
 
-    def test_empty_value(self):
-        self.assertEqual(self.fibonacci.generate(0), [])
-
-    def test_initial_value(self):
-        self.assertEqual(self.fibonacci.generate(1), [0])
-
-    def test_high_value(self):
-        self.assertEqual(self.fibonacci.generate(100)[-1], 218922995834555169026)
-
-    def test_wrong_raises(self):
-        with self.assertRaises(ValueError) as context:
-            self.fibonacci.generate("wrong")
-        self.assertEqual("Invalid input", str(context.exception))
-
-
-class TestingRecursive(unittest.TestCase):
+class TestingRecursive(AbstractFibonacci, unittest.TestCase):
 
     def setUp(self):
         self.fibonacci = FibonacciSequence(Recursive())
 
-    def test_empty_value(self):
-        self.assertEqual(self.fibonacci.generate(0), [])
-
-    def test_initial_value(self):
-        self.assertEqual(self.fibonacci.generate(1), [0])
-
-    def test_high_value(self):
-        self.assertEqual(self.fibonacci.generate(20)[-1], 4181)
-
-    def test_wrong_raises(self):
-        with self.assertRaises(ValueError) as context:
-            self.fibonacci.generate("wrong")
-        self.assertEqual("Invalid input", str(context.exception))
-
-class TestingYield(unittest.TestCase):
+class TestingYield(AbstractFibonacci, unittest.TestCase):
 
     def setUp(self):
         self.fibonacci = FibonacciSequence(Yield())
 
-    def test_empty_value(self):
-        self.assertEqual(self.fibonacci.generate(0), [])
-
-    def test_initial_value(self):
-        self.assertEqual(self.fibonacci.generate(1), [0])
-
-    def test_high_value(self):
-        self.assertEqual(self.fibonacci.generate(20)[-1], 4181)
-
-    def test_wrong_raises(self):
-        with self.assertRaises(ValueError) as context:
-            self.fibonacci.generate("wrong")
-        self.assertEqual("Invalid input", str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
